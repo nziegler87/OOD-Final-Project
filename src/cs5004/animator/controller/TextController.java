5113223ba219a5c3d7b68834f18a1c41c4f57085
Nathanial Ziegler <ziegler.n@husky.neu.edu>
2020-08-12 09:21:08 -0400

    updated contorller

--- src/cs5004/animator/controller/TextController.java
+++ src/cs5004/animator/controller/TextController.java
@@ -47,23 +47,47 @@ public class TextController implements IController {
     }
   }
 
+  /**
+   * Returns the current tick, or frame, from the controller.
+   *
+   * @return the current tick, or frame.
+   * @throws UnsupportedOperationException if called in text controller
+   */
   @Override
-  public int getCurrentFrame() {
-    return 0;
+  public int getCurrentFrame() throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("Operation not supported in text controller.");
   }
 
+  /**
+   * Sets the current tick, or frame, for the controller.
+   *
+   * @param frame the current tick, or frame.
+   * @throws UnsupportedOperationException if called in text controller
+   */
   @Override
-  public void setCurrentFrame(int frame) {
-
+  public void setCurrentFrame(int frame) throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("Operation not supported in text controller.");
   }
 
+  /**
+   * Method to start the controller's timer.
+   *
+   * @throws UnsupportedOperationException if called in text controller
+   */
   @Override
-  public void startTimer() {
-
+  public void startTimer() throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("Operation not supported in text controller.");
   }
 
+  /**
+   * Method to stop the controller's timer.
+   *
+   * @throws UnsupportedOperationException if called in text controller
+   */
   @Override
-  public void stopTimer() {
+  public void stopTimer() throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("Operation not supported in text controller.");
+
 
   }
 }
